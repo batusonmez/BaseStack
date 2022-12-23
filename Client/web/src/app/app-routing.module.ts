d3b18c83894a21236b2e2b86416c2b3a5ae55ca8
engin özdemir <xenamorphx@gmail.com>
2022-12-23 15:45:44 +0300

    rebase

--- Client/web/src/app/app-routing.module.ts
+++ Client/web/src/app/app-routing.module.ts
@@ -1,20 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'books',
-    loadChildren: () => import('./components/BookList/book.list.module').then(m => m.BookListModule)
-  },
-  {
-    path: 'edit',
-    loadChildren: () => import('./components/BookEdit/book.edit.module').then(m => m.BookEditModule)
-  },
-  {
-    path: 'view',
-    loadChildren: () => import('./components/BookView/book.view.module').then(m => m.BookViewModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
