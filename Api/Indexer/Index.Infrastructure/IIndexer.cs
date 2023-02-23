d44ee6e3add70d69beafe7f6ffac2c344a23cf3a
diwsi <xenamorphx@gmail.com>
2023-02-23 21:40:23 +0300

    Index Api

--- Api/Indexer/Index.Infrastructure/IIndexer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Index.Infrastructure
-{
-    public interface IIndexer
-    {
-        Task<bool> Index(string indexName, string id, object data);
-        Task InitIndex(string indexName);
-    }
-}
