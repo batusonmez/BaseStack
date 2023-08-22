fd472b4c662d3038ec20e3d2a23f4af4c348abdc
diwsi <xenamorphx@gmail.com>
2023-08-22 22:40:51 +0300

    Number input error fixed

--- Client/Angular.SPA/src/app/Form/FormComponents/number-input/number-input.component.ts
+++ Client/Angular.SPA/src/app/Form/FormComponents/number-input/number-input.component.ts
@@ -1,10 +1,15 @@
 import { Component, Input } from '@angular/core'; 
 import { BaseFormControl } from '../BaseFormControl';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ValidationMessageComponent } from '../../validation-message/validation-message.component';
 
 @Component({
+  standalone: true,
   selector: 'app-number-input',
   templateUrl: './number-input.component.html',
-  styleUrls: ['./number-input.component.scss']
+  styleUrls: ['./number-input.component.scss'],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, ValidationMessageComponent]
 })
 export class NumberInputComponent extends BaseFormControl {
 
